6289fe0c43dcf20dbdaa71c98e1e5bb3b0c2b09b
Asger <111696192+HARUMACHICLOWN@users.noreply.github.com>
2022-10-24 15:07:20 +0300

    Add files via upload

---
+++ lesson11/assets/js/main.js
@@ -0,0 +1,130 @@
+//alert('Я JavaScript!');
+//console.log('Я JavaScript!');
+//let name = prompt("Ваше имя", "Самир");
+//alert(`Ваше имя ${name}`);
+
+
+
+/*let a = 1, b = 1; 
+let c = ++a; 
+let d = b++;
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)*/
+
+/*let a = 2; 
+let x = 1 + (a *= 2); 
+console.log(a)
+console.log(x)*/ 
+
+/*" " + 1 + 0 
+" " - 1 + 0 
+true + false
+6 / "3"
+"2" * "3"
+4 + 5 + "px"
+"$" + 4 + 5 
+"4" - 2 
+"4px" - 2 
+"  -9  "  + 5 
+null + 1 
+undefined + 1 
+" \t \n" - 2*/ 
+
+
+/*let a = prompt("Первое число?", 1); 
+let b = prompt("Второе число?", 2); 
+
+alert(+a + +b); */
+
+
+/*let num = 3;
+alert (num)*/ 
+
+/*let a = 10;
+let b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);*/
+
+/*let c = 15;
+let d = 2;
+let result = c + d;
+alert (result);*/ 
+
+/*let a = 10; 
+let b = 2;
+let c = 5;
+alert(a + b + c)*/ 
+
+/*let a = 17; 
+let b = 10; 
+let c = a - b; 
+let d = 7; 
+let result = c + d; 
+alert(result)*/
+
+//let str = 'Привет, Мир!'
+//alert(str)
+
+/*let str1 = 'Привет, '
+let str2 = 'Мир!'
+alert(str1 + str2)*/
+
+//let name = 'Самир';
+//alert(`Привет,  ${name}`)
+
+//let age = 16; 
+//alert(`Мне  ${age} лет!!`)
+
+//let name = prompt('Какое у вас имя?'); 
+//alert( `Ваше имя ${name}`)
+
+/*let num = prompt('Назовиите число'); 
+let sum = (num ** 2)
+alert(`Квадрат этого числа ${sum}`)*/ 
+
+/*let str = 'abcde'; 
+alert(str[0]);
+alert(str[2]);
+alert(str[4]);*/
+
+/*let num = '12345'; 
+alert(num[0]*[2]*[3]*[4]*[5])*/ 
+
+
+//alert(60 * 60);
+//alert(60 * 60 * 24);
+
+
+/*let hour = 11;
+let min = 11;
+let sec = 11;
+alert(hour + ":" + min + ":" + sec)*/ 
+
+//let date = new Date()
+//alert(date)
+
+/*let num = 11; 
+let sum = num ** 2
+alert(`Квадрат числа 11 равен ${sum}`)*/ 
+
+/*var num = 47;
+num = num += 7; 
+num = num -= 18;
+num = num *= 10; 
+num = num /= 15; 
+alert(num)*/
+
+
+/*var num = 10; 
+num = num + 1; 
+num = num + 1; 
+num = num - 1;
+num ++;
+num --; 
+alert(num)*/ 
+
+
